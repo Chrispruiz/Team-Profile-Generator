15a70eb88657f7ed225f3349b2e03a16d1576d2d
Chris Ruiz <chrispruiz@att.net>
2021-04-24 22:23:58 -0500

    uploaded test for Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,45 @@
-const Employee = require('../lib/Employee.js');
-const fs = require('fs');
+const Employee = require('../lib/Employee');
 
+
+test('Can set name', () => {
+  const name = 'Chris';
+  const e = new Employee(name);
+  expect(e.name).toBe(name);
+});
+
+test('Can set id', () => {
+  const testValue = 100;
+  const e = new Employee('123', testValue);
+  expect(e.id).toBe(testValue);
+});
+
+test('Can set email', () => {
+  const testValue = 'chrispruiz@test.com';
+  const e = new Employee('123', 1, testValue);
+  expect(e.email).toBe(testValue);
+});
+
+test('Can grab name via getName()', () => {
+  const testValue = 'Chris';
+  const e = new Employee(testValue);
+  expect(e.getName()).toBe(testValue);
+});
+
+test('Can get id via getId()', () => {
+  const testValue = 100;
+  const e = new Employee('123', testValue);
+  expect(e.getId()).toBe(testValue);
+});
+
+test('Can get email via getEmail()', () => {
+  const testValue = 'Chrispruiz@test.com';
+  const e = new Employee('123', 1, testValue);
+  expect(e.getEmail()).toBe(testValue);
+});
+
+test('Test if getRole() returns \"Employee\"', () => {
+  const testValue = 'Employee';
+  const e = new Employee('Chris', 1, 'Chrispruiz@test.com');
+  expect(e.getRole()).toBe(testValue);
+});
 
